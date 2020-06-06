b732e7934957a97b6a346157f729f137496ab977
成易 <chengyi-1@alibaba-inc.com>
2020-06-06 17:40:01 +0800

    添加Javassist的案例

--- dubboDemo/src/main/java/com/sailfish/dubbo/extension/compiler/CompilerDemo.java
+++ dubboDemo/src/main/java/com/sailfish/dubbo/extension/compiler/CompilerDemo.java
@@ -1,7 +1,9 @@
 package com.sailfish.dubbo.extension.compiler;
 
-import javassist.ClassPool;
-import javassist.CtClass;
+import javassist.*;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * @author 成易
@@ -13,9 +15,16 @@ import javassist.CtClass;
  */
 public class CompilerDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CannotCompileException, IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
         final ClassPool classPool = ClassPool.getDefault();
         final CtClass ctClass = classPool.makeClass("Hello World");
+        final CtMethod ctMethod = CtNewMethod.make("public static void test(){ System.out.println(\"Hello World\");}", ctClass);
+        ctClass.addMethod(ctMethod);
+        final Class aClass = ctClass.toClass();
+
+        final Object object = aClass.newInstance();
+        final Method m = aClass.getDeclaredMethod("test", null);
+        m.invoke(object, null);
 
     }
 }
